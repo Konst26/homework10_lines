4d8cbe6ffc1d830c7dc0215226faa3f2a387c185
Константин <osokinkonstantin@mail.ru>
2024-05-18 20:50:00 +0300

    исправленные недочеты

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа");
         System.out.println("Задача 1");
-        String firstName = "ivan ";
-        String lastName = "ivanov ";
-        String middleName = "ivanovich";
-        String fullName = lastName + firstName + middleName;
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
-        System.out.println();
         System.out.println("Задача 2");
-        String small = "ivanov ivan ivanovich";
+        String small = fullName;
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + small.toUpperCase());
-        System.out.println();
         System.out.println("Задача 3");
-        String fullName2 = "Иванов Семён Семёнович";
-        String replace = fullName2.replace("ё", "e");
+        String correctedFullName = "Иванов Семён Семёнович";
+        String replace = correctedFullName.replace("ё", "e");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + replace);
     }
 }
